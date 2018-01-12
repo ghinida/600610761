daaa3b61099aca424d938823949fe771170b8f16
LazyP <35336893+ghinida@users.noreply.github.com>
2018-01-12 16:14:58 +0700

    Add files via upload

---
+++ gg.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	cout << "^*         " << endl;
+	cout << "-8         " << endl;
+	cout << "-/*        " << endl;
+	cout << "- /       " << endl;
+	cout << "-  /*      " << endl;
+	cout << "-   /     " << endl;
+	cout << "-    /*    " << endl;
+	cout << "-     /   " << endl;
+	cout << "-      /*  " << endl;
+	cout << "-     /   " << endl;
+	cout << "-    /*    " << endl;
+	cout << "-   |     " << endl;
+	cout << "-   |*     " << endl;
+	cout << "-   |     " << endl;
+	cout << "-   |*     " << endl;
+	
+	return 0;
+}
